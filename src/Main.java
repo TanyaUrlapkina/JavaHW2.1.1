57f808096cf41a134f244e67a062d9ec014db2f2
TanyaUrlapkina <write2me92@mail.ru>
2021-10-12 00:13:24 +0300

    ticket commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 2000;
+        int mile = 20;
+        int bonusMile = ticket / mile;
+
+        System.out.println(bonusMile);
+    }
+}
